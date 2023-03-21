edc0873e586ad11ce4830f87a5cd39c0be52a6a1
Jz <josegest@hotmail.com>
2023-03-21 17:54:48 -0500

    Add wrap function to plot

--- practice-folder/plotP.py
+++ practice-folder/plotP.py
@@ -1,7 +1,11 @@
 from matplotlib import pyplot as plt
 import numpy as np
-x = np.linspace(0, 2*np.pi, 1001)
-y = np.sin(x)
-m = np.linspace(0, 2*np.pi, 1001)
-n = np.sin(m)
-f = plt.plot(m,n)
+
+#wrap plot script in function
+def plot():
+    x = np.linspace(0, 2*np.pi, 1001)
+    y = np.sin(x)
+    m = np.linspace(0, 2*np.pi, 1001)
+    n = np.sin(m)
+    f = plt.plot(m,n)
+plot()
